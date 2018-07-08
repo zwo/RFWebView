d94fc0f79562b54202d7b465bda2d261d5278959
zhouweiou <zhouweiou@rfchina.com>
2018-07-08 11:59:58 +0800

    update code

--- RFWebView/Core/XWVMetaObject.h
+++ RFWebView/Core/XWVMetaObject.h
@@ -7,7 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <objc/objc-runtime.h>
+typedef NS_ENUM(NSInteger, XWVMetaObjectMember) {
+    XWVMetaObjectMemberMethod,//默认从0开始
+    XWVMetaObjectMemberProperty,
+    XWVMetaObjectMemberInitializer,
+};
+
+@interface XWVMetaObjectMember : NSObject
+@property (nonatomic, assign) XWVMetaObjectMember memberType;
+@property (nonatomic, assign) SEL selector;
+@property (nonatomic, assign) SEL getter;
+@property (nonatomic, assign) SEL selector;
+@property (nonatomic, assign) int arity;
+- (instancetype)initWithMethodSelector:(SEL)selector arity:(int)arity;
+- (instancetype)initWithPropertyGetter:(SEL)getter setter:(SEL)setter;
+- (instancetype)initWithInitializerSelector:(SEL)selector arity:(int)arity;
+- (BOOL)isMethod;
+- (BOOL)isProperty;
+- (BOOL)isInitializer;
+- (NSString*)type;
+@end
 
 @interface XWVMetaObject : NSObject
-
+@property (nonatomic, assign) Class plugin;
+@property (nonatomic, strong) NSDictionary *members;
+- (instancetype)initWithPlugin:(Class)plugin;
 @end
